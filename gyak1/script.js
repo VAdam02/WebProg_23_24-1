fcd90e8b37df960a7ad293396dcd61a5bd4c700b
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-09-11 14:57:05 +0200

    lnko

--- gyak1/script.js
+++ gyak1/script.js
@@ -8,5 +8,51 @@ console.log(szum + elvalaszto + alma);
 
 for (let i = 0; i<10; i++)
 {
+    if (i % 2 === 0)
+    {
     console.log("hello world");
+    }
+}
+
+function fahrenheitToCelsius(fahrentheit)
+{
+    return (fahrentheit -32) / 1.8;
+}
+
+const fahrenheitToCelsius2 = fahrenheitToCelsius;
+
+const fahrenheitToCelsius3 = (fahrentheit) => (fahrentheit - 32) /1.8;
+
+let fahrenheitToCelsius4 = fahrenheitToCelsius;
+
+function percentage(number, percent)
+{
+    return number* percent / 100;
+}
+
+const percentage2 = (number, percent) => number * percent / 100;
+
+
+function swap(a, b)
+{
+    let c = a;
+    a = b;
+    b = c;
+}
+
+function lnko (a, b)
+{
+    if (a < b)
+    {
+        swap(a, b);
+    }
+
+    let maradek = a % b;
+    while (maradek > 0)
+    {
+        a = b;
+        b = maradek;
+        maradek = a % b;
+    }
+    return b;
 }
