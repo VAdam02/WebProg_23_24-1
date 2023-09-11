ee81ae416ef9938461cfe61469661edbd47837ed
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-09-11 14:40:15 +0200

    for ciklus

---
+++ gyak1/script.js
@@ -0,0 +1,12 @@
+console.log("Helloo világ");
+
+const szum = 5;
+const elvalaszto = "29";
+const alma = 6;
+
+console.log(szum + elvalaszto + alma);
+
+for (let i = 0; i<10; i++)
+{
+    console.log("hello world");
+}
