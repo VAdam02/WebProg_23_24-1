c10636f77a7b9020d0ca1502952bd9ae725066a9
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-09-19 13:21:38 +0200

    szorzotabla

--- gyak2_4/script.js
+++ gyak2_4/script.js
@@ -30,3 +30,59 @@ function printHelloWorld()
     }
     helloWorldNovekvo.innerHTML = output;
 }
+
+szorzotablaCount.addEventListener("change", () => printMultiplicationTable2())
+
+function printMultiplicationTable()
+{
+    let n = szorzotablaCount.value;
+
+    if (!n.match(/^\d+$/)) return;
+
+    let output = "";
+    for (let i = 1; i <= n; i++)
+    {
+        output += `<tr>`;
+
+        for (let j = 1; j <= n; j++)
+        {
+            if (i == 1)
+            {
+                output += `<td><b>${j}</b></td>`;
+            }
+            else if (j == 1)
+            {
+                output += `<td><b>${i}</b></td>`;
+            }
+            else
+            {
+                output += `<td>${i*j}</td>`;
+            }
+        }
+
+        output += `</tr>`;
+    }
+
+    szorzotabla.innerHTML = output;
+}
+
+function printMultiplicationTable2()
+{
+    let n = szorzotablaCount.value;
+
+    if (!n.match(/^\d+$/)) return;
+
+    szorzotabla.innerHTML = "";
+
+    for (let i = 1; i <= n; i++)
+    {
+        let tr = document.createElement("tr");
+        for (let j = 1; j <= n; j++)
+        {
+            let td = document.createElement("td");
+            td.textContent = i * j;
+            tr.appendChild(td);
+        }
+        szorzotabla.appendChild(tr);
+    }
+}
