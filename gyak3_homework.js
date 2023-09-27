d5d13e75f9e674dd82f6d545b2da9b667a2f62d4
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-09-27 22:11:30 +0200

    3. gyak házifeladat

---
+++ gyak3_homework.js
@@ -0,0 +1,44 @@
+//1. válogasd ki egy tömbből a páros számokat (1 pont)
+console.log("1. feladat");
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+//TODO SOLVE HERE
+
+console.assert(compareArrays(filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8]), [2, 4, 6, 8]), "A páros számok nem jók");
+
+
+
+//2. keresd ki a legnagyobb abszolútértékű számot egy tömbből (1 pont)
+console.log("2. feladat");
+
+//TODO SOLVE HERE
+
+console.assert(findBiggestAbsoluteNumber([1, -2, 3, -4, 5, -6, 7, -8]) === -8, "A legnagyobb abszolútértékű szám nem jó");
+
+
+
+//3. hány szóból áll a leghosszabb karakterszámú névvel rendelkező személy neve? (2 pont)
+console.log("3. feladat");
+
+const names = ["Emma", "Benjamin James", "Sophia Rose", "Liam Alexander", "Olivia Grace", "Henry Joseph", "Mia Elizabeth", "Noah William", "Ava Charlotte Smith", "Ethan Daniel Johnson Williams"];
+
+//TODO SOLVE HERE
+
+console.assert(countWordsInLongestName(names) === 4, "A leghosszabb név szavainak száma nem jó");
+
+
+
+//4.a. egy gomb lenyomására írd ki consolera az oldal összes linkjének a szövegét (2 pont)
+//4.b. csak akkor írd ki, ha hosszabb a szövege, mint 10 karakter (1 pont)
+console.log("4. feladat");
+
+//TODO SOLVE HERE
+
+
+
+//5.a. Egy div-en belül hozz létre 5 db szövegdobozt (1 pont)
+//5.b. Oldd meg, hogy ha megváltoztatjuk bármelyik szövegdoboz tartalmát, akkor a többiben is változzon meg (2 pont)
+console.log("5. feladat");
+
+//TODO SOLVE HERE
