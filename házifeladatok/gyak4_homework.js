88d823272c6a9b6221c51feb130363bde398c852
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-10-04 18:39:54 +0200

    4. gyak házifeladat

---
+++ házifeladatok/gyak4_homework.js
@@ -0,0 +1,8 @@
+//Akasztófa
+//A feladat részletes leírása itt található:
+//http://webprogramozas.inf.elte.hu/#!/subjects/webprog-pti/gyak/akasztofa/akasztofa
+//Nem kötelező végig megcsinálni minden egyes feladatot a pluszpontért, mert több időt igénybe
+//tud venni az elkészítése, de egy reális mennyiséget csináljatok meg belőle és adjátok be.
+//Ez a feladat már elkezd projektben gondolkodtatni és alaposan gyakoroltatja veletek a
+//tanultakat, így javasolt foglalkozni vele, mert megkönnyítheti a beadandó elkészítésének
+//menetét.
