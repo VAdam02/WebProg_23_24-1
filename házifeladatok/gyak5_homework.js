fe8fa7d1fb4442e19f8d54b351826ea961f900bc
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-10-11 09:23:04 +0200

    5. gyak házifeladat

---
+++ házifeladatok/gyak5_homework.js
@@ -0,0 +1,11 @@
+//A KÉT FELADAT KÖZÜL ELEGENDŐ CSAK AZ EGYIKET MEGOLDANI
+
+//RÖVID FELADAT, HA EZT VÁLASZTOD, AKKOR TELJESEN MEG KELL OLDANI A PLUSZPONTÉRT
+//1. Módosítsd az órai példát, hogy ha shift lenyomása mellett kattintunk
+//az oszlopra, akkor mint csökkenő rendezési szempontként adjuk hozzá az
+//oszlopot, shift lenyomása nélkül pedig maradjon az órán megírt
+//növekvő rendezési szempont funkcionalitása
+
+//ÖSSZETETT FELADAT, HA EZT VÁLASZTOD, AKKOR ELÉG EGY REÁLIS MENNYISÉGET MEGOLDANOD A PLUSZPONTÉRT
+//2. Pixel art 5. gyakorlat 15. feladat
+//http://webprogramozas.inf.elte.hu/#!/subjects/webprog-pti/gyak/05
