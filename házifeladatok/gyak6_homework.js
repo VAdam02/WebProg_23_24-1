8ecca82f04caa92c90b095a0cf20ac993e905548
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-10-18 08:49:48 +0200

    6. gyak házifeladat

---
+++ házifeladatok/gyak6_homework.js
@@ -0,0 +1,11 @@
+//ÖSSZETETTEBB HÁZIFELADAT (ELEGENDŐ ELKEZDENI ÉS EGY REÁLIS MENNYISÉGET MEGCSINÁLNI BELŐLE ÉS NEM MUSZÁJ SORBAN HALADNI, SZABADON VÁLASZTHATÓAK A RÉSZFELADATOK)
+//1. Készíts egy Super Mariohoz hasonló játékot
+//1.a. Legyen egy karakterünk aki tud balra jobbra mozogni és ugrani egy sík pályán
+//1.b. A pálya mérete legyen szélesebb, mint a canvas és a kamera kövesse a karaktert
+//1.c. Helyezz el a pályán coinokat, amelyhez hozzáérve eltűnik és növeld a pontszámot, amit valahol tüntessünk fel
+//1.d. Csinálj ütközésvizsgálást a pályával, hogy ne csak sík tudjon lenni a pálya
+
+//EGYSZERŰBB HÁZIFELADAT (EZT TELJESEN MEG KELL CSINÁLNI HA EZT VÁLASZTOD)
+//2. Az órai flappy bird kódot (amiben az utólag felvett maradék rész is benne van) egészítsd ki a következőkkel:
+//2.a. A háttérben legyenek felhők, amik lassan mozognak jobbra a játéktérben
+//2.b. Írd ki a játékban elért aktuális pontszámot, amit a megtett távolság alapján számoljunk
