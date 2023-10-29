efbef2d457ae3ee2ee5380b20078638752958455
VAdam02 <adam.vilanyi2.program@gmail.com>
2023-10-29 14:31:51 +0100

    7. gyak házifeladat

---
+++ házifeladatok/gyak7_homework.php
@@ -0,0 +1,13 @@
+<?php
+    //1. feladat
+    //Generálj egy tömböt, amiben 10 véletlen szám van 1 és 100 között
+
+    //2. feladat
+    //Csinálj egy függvényt ami csökkenő sorrendbe rendezi a paraméterként kapott tömböt
+
+    //3. feladat
+    //Csinálj egy függvényt ami megjeleníti a paraméterként kapott tömböt
+
+    //4. feladat
+    //Az 1. feladatban létrehozott tömböt rendezd csökkenő sorrendbe a 2. feladatban létrehozott függvénnyel, majd írasd ki a 3. feladatban létrehozott függvénnyel
+?>
