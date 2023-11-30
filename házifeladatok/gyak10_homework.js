0378c2c8fd46b519c7d0299fda211925bc62a4b0
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-11-30 08:27:56 +0100

    10. gyak házifeladat

---
+++ házifeladatok/gyak10_homework.js
@@ -0,0 +1,7 @@
+/*
+A mostani hétre 2 db minta zh-ból kivett feladatot is (elég egy reális mennyiséget megoldani valamelyikből a pluszpontért, de gyakorolni mindenképpen gyakoroljatok a zh-ra, mert egy csomó kódot újra fel tudtok majd használni a zh-n)
+Az oldal elején található egy általános leírás, abban van egy kiinduló csomag
+
+3. feladat: Csomagkiszállítás (3-parcel, 8 pont)
+https://canvas.elte.hu/courses/38953/assignments/280469?module_item_id=546610
+*/
---
+++ házifeladatok/gyak10_homework.php
@@ -0,0 +1,9 @@
+<?php
+/*
+A mostani hétre 2 db minta zh-ból kivett feladatot is (elég egy reális mennyiséget megoldani valamelyikből a pluszpontért, de gyakorolni mindenképpen gyakoroljatok a zh-ra, mert egy csomó kódot újra fel tudtok majd használni a zh-n)
+Az oldal elején található egy általános leírás, abban van egy kiinduló csomag
+
+4. feladat: Receptkövető (4-recipes, 8 pont)
+https://canvas.elte.hu/courses/38953/assignments/280485?module_item_id=546691
+*/
+?>
