95202a45b418cc0b0c71ae8654dc7f2428a1f8b2
VAdam02 <61538113+VAdam02@users.noreply.github.com>
2023-11-30 09:45:34 +0100

    11. gyak házifeladat

--- házifeladatok/gyak11_homework.php
+++ házifeladatok/gyak11_homework.php
@@ -3,7 +3,7 @@
 A mostani hétre 2 db minta zh-ból kivett feladatot is (elég egy reális mennyiséget megoldani valamelyikből a pluszpontért, de gyakorolni mindenképpen gyakoroljatok a zh-ra, mert egy csomó kódot újra fel tudtok majd használni a zh-n)
 Az oldal elején található egy általános leírás, abban van egy kiinduló csomag
 
-2. feladat: Nyertél egy telefont! (2-phone, 8 pont)
+4. feladat: Receptkövető (4-recipes, 8 pont)
 https://canvas.elte.hu/courses/38953/assignments/280485?module_item_id=546691
 */
 ?>
